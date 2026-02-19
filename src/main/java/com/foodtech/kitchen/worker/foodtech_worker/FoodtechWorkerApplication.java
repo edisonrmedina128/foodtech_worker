906c89bf4254dabd2095436206453750a86bc22a
Kelvin Josue Vargas Chafle <kelvin-vargas@psofkaec013.sofka.local>
2026-02-19 11:49:05 -0500

    Initial Commit

---
+++ src/main/java/com/foodtech/kitchen/worker/foodtech_worker/FoodtechWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.foodtech.kitchen.worker.foodtech_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtechWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtechWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodtech/kitchen/worker/foodtech_worker/FoodtechWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodtech.kitchen.worker.foodtech_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtechWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
